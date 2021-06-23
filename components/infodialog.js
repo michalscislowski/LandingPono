15c9c944ac0ed66d51536f9924060de61c620a19
leisik <37248066+leisik@users.noreply.github.com>
2021-06-23 11:48:07 +0200

    Added more visible, clickable links to infodialog

--- components/infodialog.js
+++ components/infodialog.js
@@ -84,25 +84,25 @@ export default function InfoDialog() {
         </AppBar>
         <div className="main">
           <div className="box">
-            <a className="title">1. Założenie konta <a className="mb-button" onClick={() => window.open('https://www.moneybutton.com/')}>moneybutton</a></a>
+            <a className="title">1. Założenie konta moneybutton</a>
             <img></img>
-            <p className="description">Wchodzimy na <a className="mb-button" onClick={() => window.open('https://www.moneybutton.com/')}>moneybutton.com</a>. 
-              Klikamy „sign up" i wypełniamy wszystkie potrzebne do rejestracji pola. PAMIĘTAJ o potwierdzeniu emailowym, które przyjdzie do Ciebie na koniec rejestracji.</p>
+            <p className="description">Wchodzimy na <a className="mb-button-clickable" onClick={() => window.open('https://www.moneybutton.com/')}>moneybutton.com</a>. 
+              Klikamy <strong>sign up</strong> i wypełniamy wszystkie potrzebne do rejestracji pola. PAMIĘTAJ o potwierdzeniu emailowym, które przyjdzie do Ciebie na koniec rejestracji.</p>
           </div>
           <div className="box">
-            <a className="title">2. Logowanie na <a className="mb-button" onClick={() => window.open('https://tokenpow.com/')}>tokenpow.com</a></a>
+            <a className="title">2. Logowanie na tokenpow</a>
             <img></img>
-            <p className="description">Wbijamy na <a className="mb-button" onClick={() => window.open('https://tokenpow.com/')}>tokenpow.com</a>, klikamy Login i&nbsp;wybieramy Money Button - chwila magii i&nbsp;jesteśmy w miejscu docelowym.</p>
+            <p className="description">Wbijamy na <a className="mb-button-clickable" onClick={() => window.open('https://tokenpow.com/')}>tokenpow.com</a>, klikamy <strong>login</strong> i&nbsp;wybieramy Money Button - chwila magii i&nbsp;jesteśmy w miejscu docelowym.</p>
           </div>
           <div className="box">
-            <a className="title">3. Zdobycie adresu portfela <a className="mb-button" onClick={() => window.open('https://tokenpow.com/')}>tokenpow</a></a>
+            <a className="title">3. Zdobycie adresu portfela tokenpow</a>
             <img></img>
-            <p className="description">W prawym, górnym rogu ekranu kliknij na swój profil i wybierz zakładkę 'my profile'. Skopiuj adres portfela z pola 'Run Owner Address'.</p>
+            <p className="description">W prawym, górnym rogu ekranu klikamy na swój profil i wybieramy zakładkę <strong>my profile</strong>. Kopiujemy adres portfela z pola <strong>Run Owner Address</strong>.</p>
           </div>
           <div className="box">
-            <a className="title">4. Odebranie tokena na <a className="mb-button" onClick={handleClose}>wieszczemumowisietuszyfrem.pl</a></a>
+            <a className="title">4. Odebranie tokena</a>
             <img></img>
-            <p className="description">Skopiowany adres portfela wklejamy w odpowieniej rubryce razem z adresem e-mail, na który zakupiony został pre-order i klikamy ZDOBĄDŹ TOKEN!</p>
+            <p className="description">Na <a className="mb-button-clickable" onClick={handleClose}>wieszczemumowisietuszyfrem.pl</a> wklejamy w odpowienie rubryki skopiowany adres portfela razem z&nbsp;adresem email, na który zakupiony został pre-order i klikamy <strong>ZDOBĄDŹ TOKEN!</strong></p>
           </div>
           <div>
             <Button 
@@ -143,12 +143,13 @@ export default function InfoDialog() {
           line-height: 1.75rem;
           text-align: justify;
         }
-        .mb-button {
+        .mb-button-clickable {
           cursor: pointer;
           font-weight: 700;
+          color: #0b7a75;
         }
-        .mb-button:hover {
-          color: #ff893f;
+        .mb-button-clickable:hover {
+          color: #2c9c97;
         }
         @media only screen and (max-width: 499px) {
           .box {
